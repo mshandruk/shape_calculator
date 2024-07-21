f7d15dea51daf094e87ccf2fff347893b1f296b3
Maxim Shandruk <mxshandruk@gmail.com>
2024-07-21 22:37:19 +0300

    Create client code for Rectangle calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use shape_calculator::{ShapeAreaCalculator, Rectangle};
+
 fn main() {
-    println!("Hello, world!");
+    let rectangle = Rectangle {
+        width: 20,
+        height: 30,
+    };
+    let rectangle_area = rectangle.calculate_area();
+    println!("Rectangle area is: {}", rectangle_area);
 }
