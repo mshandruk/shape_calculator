1e60288f1cae963e33130240fe9532aea37ba070
Maxim Shandruk <mxshandruk@gmail.com>
2024-07-21 22:36:33 +0300

    Implement ShapeAreaCalculator for Rectangle

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,10 @@ pub struct Rectangle {
     pub width: u32,
     pub height: u32,
 }
+
+
+impl ShapeAreaCalculator for Rectangle {
+    fn calculate_area(&self) -> u32 {
+        self.width * self.height
+    }
+}
